cdb79689fab3a1819585d35871049f6bf3ab9651
Nathaniel J. Smith <njs@pobox.com>
2013-09-26 13:51:01 +0100

    again

--- pyrerp/__init__.py
+++ pyrerp/__init__.py
@@ -2,12 +2,6 @@
 # Copyright (C) 2012-2013 Nathaniel Smith <njs@pobox.com>
 # See file COPYING for license information.
 
-__version__ = "0.0.0+dev"
-
-# Do this first, to make it easy to check for warnings while testing:
-import os
-if os.environ.get("PYRERP_FORCE_NO_WARNINGS"):
-    import warnings
-    warnings.filterwarnings("error", module="^pyrerp")
-    del warnings
-del os
+import warnings
+warnings.filterwarnings("error", module="^pyrerp")
+del warnings
---
+++ pyrerp/test_bar.py
@@ -0,0 +1,8 @@
+import numpy as np
+from scipy import sparse
+a = np.zeros((2, 2))
+b = sparse.csc_matrix([[10.0, 45.0], [45.0, 285.0]])
+print repr(a)
+print repr(b)
+a + b
+a += b
